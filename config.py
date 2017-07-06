f843cc69e03fdedfd8af7f89d1a888a77304e97f
Mateusz Loskot <mateusz@loskot.net>
2017-07-07 00:58:25 +0200

    Enter of the multi-environment configurations

--- config.py
+++ config.py
@@ -1,46 +1,32 @@
 class Config(object):
+    """Common settings.
     """
-    Common configurations
-    """
-
-    # Put any configurations here that are common across all environments
-
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = 'you-really-need-to-change-this'
     SQLALCHEMY_POOL_RECYCLE = 499
     SQLALCHEMY_POOL_TIMEOUT = 20
 
 class DevelopmentConfig(Config):
+    """Development (local) environment specific settings.
     """
-    Development configurations
-    """
-
     DEBUG = True
+    TESTING = True
     SQLALCHEMY_ECHO = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite://development.db'
 
+class StagingConfig(Config):
+    """Pre-production specific settings.
+    """
+    DEBUG = True
 
 class ProductionConfig(Config):
+    """Production specific settings.
     """
-    Production configurations
-    """
-
     DEBUG = False
 
 app_config = {
     'development': DevelopmentConfig,
+    'staging': StagingConfig,
     'production': ProductionConfig
 }
-
-
-# app.config['OAUTH_CREDENTIALS'= {
-#     'facebook': {
-#         'id': '837483683053553',
-#         'secret': 'bce2321650fbb754b955453e1d10b2f0'
-#     },
-#     'google': {
-#         'id': '838769494183-f2e9i6s0ng8eps3075bfsmlgoo3e7hvk.apps.googleusercontent.com',
-#         'secret': '51V_GgfoNr52oB0X9vhVXd3E'
-#     },
-#     'twitter': {
-#         'id': 'dDu5qMg32Rsh94P89UCcQoLJO',
-#         'secret': 'EB6LF33hBYjhKsiPqxofG1NN8PFSQ1i007zjdIvDekgCnkbkp7'
-#     }
-# }
