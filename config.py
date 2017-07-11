d8d3eba72db78dce836a550da676b15f8cfcae9a
Mateusz Loskot <mateusz@loskot.net>
2017-07-12 00:18:18 +0200

    Add TestingConfig. Rationalise config of dev/testing DB.

--- config.py
+++ config.py
@@ -2,6 +2,8 @@
 """
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
     """Common settings.
     """
@@ -15,20 +17,29 @@ class DevelopmentConfig(Config):
     """Development (local) environment specific settings.
     """
     DEBUG = True
+    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'kampuswerk-dev.sqlite')
+
+class TestingConfig(Config):
+    """Development (local) environment specific settings.
+    """
     TESTING = True
     SQLALCHEMY_ECHO = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite://development.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'kampuswerk-test.sqlite')
 
 class StagingConfig(Config):
     """Pre-production specific settings.
     """
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('STAGE_DATABASE_URL')
 
 class ProductionConfig(Config):
     """Production specific settings.
     """
     DEBUG = False
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 app_config = {
     'development': DevelopmentConfig,
